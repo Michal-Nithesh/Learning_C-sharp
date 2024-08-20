959cbfde4be41bdcf3716fa91956c9ee641c7f1c
Nithesh14 <michalnithesh@gmail.com>
2024-08-20 11:43:38 +0530

    Learn upto Overview

---
+++ Overview/Comments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        /// <parm name="args"> An array of string arguments from the command line </parm>
+        /// <return>
+        /// No return value
+        /// </return>
+        static void Main(string[] args)
+        {
+            // Single line comment start with two slashes
+            Console.WriteLine("Hello World!");
+            /* Multiple line comments start with a slash and a star
+            and can continue for several lines
+            untile a closing star and slash are encountered*/
+        }
+    }
+}
+
---
+++ Overview/Operators/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Declare some variables to excercise the operators
+            int x = 10 , y = 11;
+            string a = "Michal", b = "Nithesh";
+
+            // Basic math Operators are +, -, /, *
+            Console.WriteLine("-----Basic Math-----");
+            Console.WriteLine((x * y) / x);
+            Console.WriteLine(a + b);
+
+
+            // Increment / Decrement operators
+            Console.WriteLine("-----Shorthand-----");
+            x++; //Increment
+            y--; //Decrement
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            // Operators can be shorthand: a = a + b
+            a += b;
+            Console.WriteLine(a);
+
+            
+            // TODO: Logical operators &&, ||
+            Console.WriteLine("----- Logic Operators -----");
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
+
+
+            // null-coalescing operators
+            string str = null;
+            // TODO: the ?? operator uses left operand if not null, or right one if it is
+            Console.WriteLine(str ?? "Unknown String");
+
+            
+            // TODO: the ??= operator assigns the right operand if the left one is null
+            // it replaces the code:
+            // if (variable is null) {
+            //    variable = somevalue;
+            // }
+            str ??= "New String";
+            Console.WriteLine(str);
+        }
+    }
+}
